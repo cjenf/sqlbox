2d779b8170450212eb89ae01b6accfbbba6d470d
Cjenf <kimichen2907@gmail.com>
2024-07-21 14:48:43 +0800

    sql

---
+++ creat_db.py
@@ -0,0 +1,32 @@
+import sqlite3
+import os
+
+class Database:
+    def __init__(self, *, database:str) -> None:
+        self .database=database
+        
+    def _creat_database(self) -> None:
+        """
+        Connects to the specified database, creates a cursor, and then closes the cursor if the database name is provided. 
+        Raises a NamingError with the message "Database name must be str" if the database name is not provided.
+        """
+        if self.database:
+            self.con = sqlite3.connect(self.database)
+            self.cur = self.con.cursor()
+            self.cur.close()
+
+        else:
+            import exception_
+            raise exception_.NamingError("Database name must be str")
+        
+    def _connect_database(self) -> None: 
+        """
+        Connects to the specified database, creates a cursor, and then closes the cursor if the database name is provided. 
+        Raises a DatabaseNotFoundError if the database file is not found.
+        """   
+        if os.path.exists(file:=f'{self.database}'):
+            self.con = sqlite3.connect(file)
+            self.cur = self.con.cursor()
+        else:
+            import exception_
+            raise exception_.DatabaseNotFoundError(f"{file} not found")
